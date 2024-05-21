34dedc186d4024048c844fec212170d1c122f7c7
status102 <102887808+status102@users.noreply.github.com>
2024-05-21 10:01:24 +0800

    perf: 换了一个方式实现的Toast通知

--- DailyRoutines/Notifications/WinToast.cs
+++ DailyRoutines/Notifications/WinToast.cs
@@ -5,22 +5,14 @@ using System.Windows.Forms;
 using DailyRoutines.Helpers;
 using DailyRoutines.Managers;
 using ECommons.Automation;
+using Microsoft.Toolkit.Uwp.Notifications;
 
 namespace DailyRoutines.Notifications;
 
 public class WinToast : DailyNotificationBase
 {
-    private class ToastMessage(string? title, string message, ToolTipIcon icon = ToolTipIcon.Info)
-    {
-        public string? Title { get; set; } = title;
-        public string Message { get; set; } = message;
-        public ToolTipIcon Icon { get; set; } = icon;
-    }
 
     private static TaskManager? TaskManager;
-
-    private static NotifyIcon? icon;
-    private static readonly Queue<ToastMessage> messagesQueue = new();
 
     public override void Init()
     {
@@ -29,67 +21,14 @@ public class WinToast : DailyNotificationBase
 
     public static void Notify(string title, string content, ToolTipIcon toolTipIcon = ToolTipIcon.Info)
     {
-        if (icon == null || icon.Visible == false) CreateIcon();
-        messagesQueue.Enqueue(new ToastMessage(title, content, toolTipIcon));
-
-        TaskManager.DelayNext(200);
-        TaskManager.Enqueue(TryDequeueMessages);
-    }
-
-    private static void TryDequeueMessages()
-    {
-        switch (messagesQueue.Count)
-        {
-            case 0:
-                DestroyIcon();
-                break;
-            case 1:
-                ShowBalloonTip(messagesQueue.Dequeue());
-
-                TaskManager.DelayNext(6000);
-                TaskManager.Enqueue(TryDequeueMessages);
-                break;
-            case >= 2:
-                ShowBalloonTip(new ToastMessage("", Service.Lang.GetText("NotificationManager-ReceiveMultipleMessages", messagesQueue.Count)));
-                messagesQueue.Clear();
-
-                TaskManager.DelayNext(6000);
-                TaskManager.Enqueue(TryDequeueMessages);
-                break;
-        }
-    }
-
-    private static void ShowBalloonTip(ToastMessage message)
-    {
-        icon.ShowBalloonTip(
-            5000, string.IsNullOrEmpty(message.Title) ? Plugin.Name : SanitizeHelper.Sanitize(message.Title),
-            SanitizeHelper.Sanitize(message.Message), message.Icon);
-    }
-
-    private static void CreateIcon()
-    {
-        DestroyIcon();
-        icon = new NotifyIcon
-        {
-            Icon = new Icon(Path.Join(Service.PluginInterface.AssemblyLocation.DirectoryName, "Assets", "FFXIVICON.ico")),
-            Text = Plugin.Name,
-            Visible = true
-        };
-    }
-
-    private static void DestroyIcon()
-    {
-        if (icon != null)
-        {
-            icon.Visible = false;
-            icon.Dispose();
-            icon = null;
-        }
+        new ToastContentBuilder()
+            .AddText(title)
+            .AddText(content)
+            .Show();
     }
 
     public override void Uninit()
     {
-        DestroyIcon();
         TaskManager?.Abort();
     }
 }
