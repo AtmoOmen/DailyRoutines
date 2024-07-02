6411080d7573c139b12ad380f8462186b05ff57e
AtmoOmen <atmoomen2004@outlook.com>
2024-07-02 19:03:33 +0800

    Fix Talk Skip

--- DailyRoutines/Modules/UIOperation/AutoTalkSkip.cs
+++ DailyRoutines/Modules/UIOperation/AutoTalkSkip.cs
@@ -1,7 +1,8 @@
-using ClickLib;
+using DailyRoutines.Infos;
 using DailyRoutines.Managers;
 using Dalamud.Game.Addon.Lifecycle;
 using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
+using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace DailyRoutines.Modules;
 
@@ -12,10 +13,21 @@ public class AutoTalkSkip : DailyModuleBase
 
     public override void ConfigUI() { ConflictKeyText(); }
 
-    private static void OnAddonDraw(AddonEvent type, AddonArgs args)
+    private static unsafe void OnAddonDraw(AddonEvent type, AddonArgs args)
     {
         if (Service.KeyState[Service.Config.ConflictKey]) return;
-        Click.SendClick("talk");
+        var addon = args.Addon.ToAtkUnitBase();
+        var evt = stackalloc AtkEvent[1]
+        {
+            new()
+            {
+                Listener = (AtkEventListener*)addon,
+                Flags = 132,
+                Target = &AtkStage.Instance()->AtkEventTarget,
+            },
+        };
+        var data = stackalloc AtkEventData[1];
+        addon->ReceiveEvent(AtkEventType.MouseClick, 0, evt, data);
     }
 
     public override void Uninit()
